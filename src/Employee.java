3664c66d5549e460da7b37c9ef0a8c9060cc3eb0
June <june122@inbox.ru>
2022-12-14 17:25:56 +0300

    первый этап сделан, надеюсь что всё верно...

--- src/Employee.java
+++ src/Employee.java
@@ -8,53 +8,53 @@ public class Employee {
     private int salary;
     private static int counter;
     private int id;
-    public String getFio() {
+    public String getFio(){
         return fio;
     }
 
-    public int getOffice() {
+    public int getOffice(){
         return office;
     }
 
-    public int getSalary() {
+    public int getSalary(){
         return salary;
     }
 
-    private static int getCounter() {
+    private static int getCounter(){
         return counter;
     }
 
-    private int getId() {
+    private int getId(){
         return id;
     }
 
-    private void setOffice( int office ) {
+    private void setOffice( int office ){
         this.office = office;
     }
 
-    private void setSalary( int salary ) {
+    private void setSalary( int salary ){
         this.salary = salary;
     }
 
-    public Employee( String fio, int office, int salary, int id ) {
+    public Employee( String fio, int office, int salary, int id ){
         this.fio = fio;
         this.office = office;
         this.salary = salary;
         this.id = counter++;
     }
     @Override
-    public String toString() {
+    public String toString(){
         return "Сотрудник: " + fio + "\nОтдел - " + office + "\nЗарплата - " + salary + "\nid - " + id;
     }
     @Override
-    public boolean equals( Object o ) {
+    public boolean equals( Object o ){
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Employee employee = (Employee) o;
         return office == employee.office && salary == employee.salary && id == employee.id && Objects.equals(fio, employee.fio);
     }
     @Override
-    public int hashCode() {
+    public int hashCode(){
         return Objects.hash(fio, office, salary, id);
     }
 }
